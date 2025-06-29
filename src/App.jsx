841ca4d3471d4796062fb7613b1bc81a3175135f
Courage Ganyiwa <couresma@gmail.com>
2025-06-29 08:15:21 +0200

    latest changes to the Header and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="hero-image d-flex align-items-center justify-content-center"
+        style={{
+          backgroundImage:
+            'url("./src/assets/white-modern-house-curved-patio-archway-c0a4a3b3-aa51b24d14d0464ea15d36e05aa85ac9.webp")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "100vw",
+          height: "100vh",
+          marginTop: "55px",
+          padding: 0,
+        }}
+      >
+        <button className="btn btn-lg btn-danger">Learn More</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import ProductList from "./components/ProductList.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <Header />
     <App />
     <ProductList />
   </StrictMode>
