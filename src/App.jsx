c66ca35194b5aae5fee7fa1a486400b8f7f30a2b
Courage Ganyiwa <couresma@gmail.com>
2025-06-29 10:27:12 +0200

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,44 @@ function App() {
           padding: 0,
         }}
       >
-        <button className="btn btn-lg btn-danger">Learn More</button>
+        <div class="card bg-dark">
+          <h5 class="card-header bg-white bg-gradient">Featured</h5>
+          <div class="card-body text-white">
+            <h5 class="card-title">Secure Your Home</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-danger">
+              <i className="bi bi-arrow-right-circle-fill"></i> Explore Now
+            </a>
+          </div>
+        </div>
       </div>
+      <section className="container-fluid row d-flex justify-content-center align-items-center">
+        <div className="col-md-6 d-flex justify-content-center align-items-center p-5">
+          <img
+            src={
+              "./src/assets/3D security verification icon isolated on transparent background.jpg"
+            }
+            alt=""
+            className="img-fluid w-100 h-100 rounded"
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
+        </div>
+        <div className="col-md-6 text-white bg-dark p-5 d-flex flex-column justify-content-center align-items-center">
+          <p className=" mb-4 text-light">
+            <span className="text-warning text-uppercase fs-lg fw-bold">
+              Remote Guru
+            </span>{" "}
+            is your go-to platform for finding home security products and
+            services. Whether you're looking for the latest security cameras,
+            smart locks, or professional installation services, we've got you
+            covered.
+          </p>
+          <button className="btn btn-danger rounded">Read More</button>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -5,26 +5,56 @@ import React, { useEffect, useState } from "react";
 function ProductList() {
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/products/")
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error("Error fetching products:", error));
-  }, []);
-
-  console.log(products);
+  // useEffect(() => {
+  //   fetch("http://127.0.0.1:8000/api/products/")
+  //     .then((response) => response.json())
+  //     .then((data) => setProducts(data))
+  //     .catch((error) => console.error("Error fetching products:", error));
+  // }, []);
 
   return (
     <div>
-      <h2>Product List</h2>
-      <ul>
-        {products.map(({ id, image, price, name }) => (
-          <li key={id}>
-            <img src={`http://127.0.0.1:8000/${image}`} alt="image" />
-            {name} - ${price}
-          </li>
-        ))}
-      </ul>
+      <section className="bg-white p-4">
+        <h2 className="mb-4 text-dark">Product List</h2>
+        <div className="container-fluid row d-flex justify-content-evenly align-items-center">
+          <div className="card col-5 bg-dark text-light">
+            <img
+              src={
+                "./src/assets/3D security verification icon isolated on transparent background (1).jpg"
+              }
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+          <div className="card col-5 bg-dark text-light">
+            <img
+              src={
+                "./src/assets/3D security verification icon isolated on transparent background (1).jpg"
+              }
+              className="card-img-top"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
