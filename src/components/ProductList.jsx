b1c3beb39b3ca113cda19332d0eb820e8f2a3acd
Courage Ganyiwa <couresma@gmail.com>
2025-07-01 16:13:11 +0200

    latest changes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,7 @@
 // Example: src/components/ProductList.js
 
 import React, { useEffect, useState } from "react";
-const pic1 =
-  ".\\src\\assets\\3D security verification icon isolated on transparent background (1).jpg";
+import pic2 from "../assets/3D security verification icon isolated on transparent background (1).jpg";
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -20,7 +19,7 @@ function ProductList() {
         <h2 className="mb-4 text-dark">Products and Services</h2>
         <div className="container-fluid row d-flex justify-content-evenly align-items-center flex-wrap">
           <div className="card col-md-6 col-12 text-light border-0">
-            <img src={pic1} className="card-img-top" />
+            <img src={pic2} className="card-img-top" />
             <div className="card-body bg-dark ">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
@@ -33,7 +32,7 @@ function ProductList() {
             </div>
           </div>
           <div className="card col-md-6 col-12 text-light border-0">
-            <img src={pic1} className="card-img-top" />
+            <img src={pic2} className="card-img-top" />
             <div className="card-body bg-dark ">
               <h5 className="card-title">Card title</h5>
               <p className="card-text">
