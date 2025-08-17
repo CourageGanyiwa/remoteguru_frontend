b204b7c23f405924b9a90a17efa2a92c5a779a2a
Courage Ganyiwa <couresma@gmail.com>
2025-08-18 00:10:52 +0200

    latest changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,20 @@
+import logo from "../assets/remotegurulogo_0x90.jpg";
+
 export default function Footer() {
   return (
     <>
       <footer className="d-flex flex-wrap align-items-center py-3 my-4 border-top">
-        <p className="col-md-4 mb-0 text-light">&copy;2025 Remote Guru, Inc</p>
+        <p className="col-md-4 mb-0 text-light text-center text-md-start">
+          &copy;2025 Remote Guru, Inc
+        </p>
         <a
           href="/"
           className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
           aria-label="RG Logo"
         >
-          {/* <svg className="bi me-2" width="40" height="32" aria-hidden="true">
-              <img src="../assets/remotegurulogo_0x90.jpg" alt="" />
-            </svg> */}
+          <div className="bi me-1" width="30" height="22" aria-hidden="true">
+            {/* <img src={logo} alt="logo" cla/> */}
+          </div>
         </a>
         <ul className="nav col-md-4 justify-content-end">
           <li className="nav-item">
